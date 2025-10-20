b540c60e25ce0844336d00a0aef628e8a04b31d4
ifndefy <lee.sjoe.3@gmail.com>
2025-10-19 23:19:29 -0700

    Fix merge conflict

---
+++ services/database_service.py
@@ -0,0 +1,10 @@
+from services import connectDB as db
+
+class DatabaseService:
+    def __init__(self):
+        print("Database init")
+
+    def connect(self):
+        self.connection = db.DatabaseConnection()
+        self.connection.establish_connection()
+        return self.connection
