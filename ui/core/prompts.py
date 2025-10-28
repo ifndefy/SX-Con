235bf56c72496d3eb818c1ba6dd7b51c8b8c2ebd
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2025-10-28 12:03:18 -0700

    Update prompts.py
    Updated hash_security_question_answer() to hash both the question and answer

--- ui/core/prompts.py
+++ ui/core/prompts.py
@@ -1,18 +1,18 @@
 from core import hash_password
 
 def hash_security_question_answer(question_answer_pair : dict[str, str]) -> dict[str, str]:
+    # dictionary is key,value pair. key = question, value = answer
     if not question_answer_pair:
         return -1
-    # dictionary is key,value pair. key = question, value = answer
 
     hashed_question_answer_pair = {}
     # Hash all answers (values)
     for key, value in question_answer_pair.items():
         if value == None:
             return -1
-
         hashed_answer = hash_password.hash_password(value)
-        hashed_question_answer_pair[key] = hashed_answer
+        hashed_question = hash_password.hash_password(key)
+        hashed_question_answer_pair[hashed_question] = hashed_answer
 
     return hashed_question_answer_pair
 
