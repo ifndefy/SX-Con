fe486b269422d203069c3766da48377dd9af0002
ifndefy <lee.sjoe.3@gmail.com>
2025-10-27 22:39:01 -0700

    SXC-20: implement get_max_value.py to select max value of an attribute + called in post.py for ticket_num

---
+++ services/get_max_value.py
@@ -0,0 +1,23 @@
+from services.database_service import DatabaseService
+
+def get_max_value(table_name, column_name):
+    db_service = DatabaseService()
+    db_service.connect()
+
+    try:
+        if db_service.connection is None:
+            print("No connection established")
+            return -1
+
+        cursor = db_service.connection.create_cursor()
+        query = f"SELECT MAX({column_name}) FROM {table_name}"
+        cursor.execute(query)
+        result = cursor.fetchone()
+
+        if result[0] is not None:
+            return result[0]
+        else:
+            return 0
+    finally:
+        if db_service.connection:
+            db_service.connection.close_connection()
