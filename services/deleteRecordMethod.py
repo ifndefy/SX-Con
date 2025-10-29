f5a07053010eabc7e8f05f73497936235f7c1024
Tim Liu <timliu@csus.edu>
2025-10-29 13:24:23 -0700

    -fixxed the return value placement -moved the connection close statement to finally -added a debug log when an exception is hit -the unused str will default to '' so its not a mandatory

--- services/deleteRecordMethod.py
+++ services/deleteRecordMethod.py
@@ -1,22 +1,24 @@
 from database_service import DatabaseService as Ds
 
-def delete_record(table_name: str, key: str, unused: str) -> int :
+def delete_record(table_name: str, key: str, unused: str = '') -> int :
     """"
     This function deletes a record from a table
 
     @param table_name: name of the table
     @param key: the location on the table
-    @param unused: the unused value
+    @param unused: the unused value, defaults to ''
     @return: 0 on success -1 on failure
     """
     try:
         print("deleting a record")
-        conn = Ds.connect()
+        conn = Ds.connection()
         cursor = conn.cursor()
         cursor.execute("DELETE FROM " + table_name + " WHERE " + key)
 
-        conn.close()
+        return 0
     except Exception as e:
+        print("ERROR: failed to delete " + str(e))
         return -1
     finally:
-        return 0
+        if conn:
+            conn.close_connection()
