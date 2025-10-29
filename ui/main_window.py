df30dc0fb5ca41bc4b913443081beda929a1ad74
ifndefy <lee.sjoe.3@gmail.com>
2025-10-28 21:28:00 -0700

    SXC-119: Refactored "post.py" GUI + updated minimum window size + added QSS for new status section

--- ui/main_window.py
+++ ui/main_window.py
@@ -35,8 +35,8 @@ class MainWindow(QWidget):
 
     def setup_window(self):
         self.setWindowTitle("SX-Con")
-        self.setGeometry(0, 0, 1100, 786)
-        self.setMinimumSize(1100, 786)
+        self.setGeometry(0, 0, 1100, 756)
+        self.setMinimumSize(1100, 756)
         # todo: Resize height to fit Super X logo when delivered to developers
 
     def setup_ui(self):
