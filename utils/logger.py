cb2ceec56cc8bbbbc9a6546cecb84fa7937c004b
MK0mar0v <110884056+MK0mar0v@users.noreply.github.com>
2025-10-30 19:53:10 -0700

    first implementation of logger (#32)

---
+++ utils/logger.py
@@ -0,0 +1,55 @@
+import configparser as cparser
+import logging as log
+import logging.config as config
+from pathlib import Path
+
+#Logger setup to be done on import
+#Create object to read in config file
+config = cparser.ConfigParser()
+
+config.read("config.ini")
+
+#Get logger config values from file
+__path = config.get('Logger Settings', 'log_path')
+__name = config.get('Logger Settings', 'log_name')
+__format = config.get('Logger Settings', 'log_format', raw = True)
+__level_str = config.get('Logger Settings', 'log_level')
+
+#convert level string to level attribute if exists
+__level_attr = getattr(log, __level_str, log.CRITICAL)
+
+#Setup values for log handler and format
+__log_location = Path(__path + __name) 
+__log_format = log.Formatter(__format) 
+
+#Grab logger object from interpreter and create/open log file 
+#(all modules share same logger obejct, can be changed in the future)
+__app_logger = log.getLogger('base_logger')
+__log_handler = log.FileHandler(__log_location, 'w')
+
+#Set handler format and attach to logger object
+__log_handler.setFormatter(__log_format)
+__app_logger.addHandler(__log_handler)
+
+__app_logger.setLevel(__level_attr)
+
+#Actual logging functions
+def debug(msg: str):
+    __app_logger.debug(msg, stacklevel = 2)
+    return
+
+def error(msg: str):
+    __app_logger.error(msg, stacklevel = 2)
+    return
+
+def info(msg: str):
+    __app_logger.info(msg, stacklevel = 2)
+    return
+
+def warning(msg: str):
+    __app_logger.warning(msg, stacklevel = 2)
+    return
+
+def critical(msg: str):
+    __app_logger.critical(msg, stacklevel = 2)
+    return
