14abb7fa60ef1af0a5f8c9806420da7cfa1cd899
ifndefy <lee.sjoe.3@gmail.com>
2025-11-11 22:30:45 -0800

    SXC-142: Change to Cosmos DB

--- services/connectDB.py
+++ services/connectDB.py
@@ -1,97 +1,49 @@
 import configparser as cparser
-import pyodbc as odbc
-import struct
-from azure.identity import DefaultAzureCredential
+
+from azure.cosmos import CosmosClient
 from pathlib import Path
 
 
 class DatabaseConnection:
     def __init__(self):
-        print("Initializing connectDB defaults...")
-
-        self.db_connection = None
+        print("Initializing Cosmos DB connection...")
+        self.client = None
+        self.database = None
+        self.container = None
 
         config = cparser.ConfigParser()
-
         current_dir = Path(__file__).parent
         config_path = current_dir / 'config.ini'
 
         try:
             config.read(config_path)
-
-            self.odbc_driver = config.get('SQL Connection Parameters', 'odbc_driver')
-            self.server_addr = config.get('SQL Connection Parameters', 'server_addr')
-            self.server_port = config.get('SQL Connection Parameters', 'server_port')
-            self.db_name_default = config.get('SQL Connection Parameters', 'db_name')
-            self.sql_access_token = config.getint('SQL Connection Parameters','sql_access_token')
+            self.endpoint = config.get('Cosmos Connection Parameters', 'endpoint')
+            self.database_name = config.get('Cosmos Connection Parameters', 'database_name')
+            self.container_name = config.get('Cosmos Connection Parameters', 'container_name')
+            self.key = config.get('Cosmos Connection Parameters', 'key')
         except Exception as e:
             print(f"Error fetching from config.ini: {e}")
 
-    def establish_connection(self, 
-                            driver = None, 
-                            addr = None, 
-                            port = None, 
-                            db_name = None, 
-                            access_mode = None):
+    def establish_connection(self, endpoint=None, database_name=None, container_name=None, key=None):
         """
-        Establishes a connection with the Azure SQL Database.
-
-        [Args]: All arguments are optional, args not given will be fetched from default values in config.ini\n
-            driver (string): Specifies the odbc driver to use for the connection, string in format \'{[driver name]}\'
-            addr (string): Address of the server
-            port (string): Server port being used
-            db_name (string): Name of the specific database on the server
-            db_user (string): The user connecting to the server, must exist as a valid user in the db
-            access_mode (int): used to identify what form of authentication is being used, almost no reason to ever change  
-        
-        On success return 0, connection is established and stored in self.db_connection can be used to create cursors. Failures return -1 and no values are written to self.db_connection    
+        :purpose: Connect the object to the database to enable communications between the program and the database
+        :author(s): Maksym Komarov, Joe Lee
         """
+        endpoint = endpoint or self.endpoint
+        database_name = database_name or self.database_name
+        container_name = container_name or self.container_name
+        key = key or self.key
 
-        #if any specific values were not used, load from config instead 
-        driver = driver or self.odbc_driver
-        addr = addr or self.server_addr
-        port = port or self.server_port
-        db_name = db_name or self.db_name_default
-        access_mode = access_mode or self.sql_access_token
-
-        #Check if any connection string fields are null
-        if not all([driver, addr, port, db_name, access_mode]):
-            print("one or more required fields are null")
+        if not all([endpoint, database_name, container_name, key]):
+            print("One or more required fields are null")
             return -1
-        credential = DefaultAzureCredential(
-            exclude_interactive_browser_credential=True
-        )
-        connection_string = f"Driver={driver};" \
-                            f"Server={addr},{port};" \
-                            f"Database={db_name};" \
-                            "Encrypt=yes;" \
-                            "TrustServerCertificate=no;" \
-                            "Connection Timeout=30"
-
-        token = credential.get_token("https://database.windows.net/.default")
-        token_bytes = token.token.encode("UTF-16-LE")
-
-        token_struct = struct.pack(f'<I{len(token_bytes)}s', 
-                                   len(token_bytes), 
-                                   token_bytes)
 
         try:
-            self.db_connection = odbc.connect(connection_string, attrs_before={access_mode: token_struct})
-        except:
-            print("Failed to establish connection")
+            self.client = CosmosClient(url=endpoint, credential=key)
+            self.database = self.client.get_database_client(database_name)
+            self.container = self.database.get_container_client(container_name)
+            print("Cosmos DB connection established")
+            return 0
+        except Exception as e:
+            print(f"Failed to establish Cosmos DB connection: {e}")
             return -1
-        
-        return 0
-
-    def create_cursor(self):
-        if self.db_connection is not None:
-            return self.db_connection.cursor()
-        else:
-            print("Connection not established")
-
-    def close_connection(self):
-        if self.db_connection is not None:
-            print("Closing connection")
-            self.db_connection.close()
-        else:
-            print("Connection not found")
