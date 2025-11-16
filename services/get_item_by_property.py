688babc0e77538b469d4330672bd1a6e6d3d92f7
Joe <69750727+ifndefy@users.noreply.github.com>
2025-11-15 23:59:53 -0800

    Merge pull request #62
    * SXC-170: implements autopopulation when phone is entered + implements…
    
    * SXC-170: implements autopopulation when phone is entered + implements…

---
+++ services/get_item_by_property.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+from services.connect_database import db_connection
+
+
+def get_item_by_property(container_name: str, entity_type: str, property_name: str, property_value: str) -> Any | None:
+    """
+    :purpose: gets an item by property
+    :param container_name: name of the container
+    :param entity_type: type of the entity
+    :param property_name: name of the propertyW
+    :param property_value: value of the property
+    :return: None
+    :author(s): Joe Lee
+    """
+    try:
+        container = db_connection.connect(container_name)
+
+        query = f"SELECT * FROM c WHERE c.type = '{entity_type}' AND c.{property_name} = '{property_value}'"
+
+        items = list(container.query_items(
+            query=query,
+            enable_cross_partition_query=True
+        ))
+
+        if items:
+            print(f"Cosmos DB Container {container_name} found by {property_name}: {property_value}")
+            return items[0]
+        else:
+            print(f"Cosmos DB container {container_name} not found by {property_name}: {property_value}")
+            return None
+    except Exception as e:
+        print(f"Error in get_item_by_property: {e}")
+        return None
