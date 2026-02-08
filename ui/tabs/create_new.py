4746c11aa1b7fcb45183fdf5528a86f100b84faa
KyleCsus <cvaldez3@csus.edu>
2026-02-08 15:12:48 -0800

    SXC-104-Implemented restrict policy and modified format price (#86)
    * SXC-104-Implemented restrict policy and modified format price
    
    * SXC-104-Added restricted format price in create_new.py
    
    ---------
    
    Co-authored-by: Kyle Valdez <kyle.valdez@risestemup.com>

--- ui/tabs/create_new.py
+++ ui/tabs/create_new.py
@@ -18,6 +18,7 @@ from ui.core.autogen_date import generate_host_datetime
 from ui.core.autogen_ticket_num import autogen_ticket_num
 from ui.core.revenue_generation import RevenueGeneration
 from ui.core import format_phone
+from ui.core.format_price import restricted_format_price
 
 BASE_RATE = 25
 
@@ -411,6 +412,9 @@ class CreateNewTab(BaseTab):
         price_input = QLineEdit()
         price_input.setPlaceholderText(f"$0.00")
         price_input.setFixedWidth(100)
+        
+        restricted_format_price(price_input)
+
         line2_layout.addWidget(price_input)
         product_section['price'] = price_input
 
