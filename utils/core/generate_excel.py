d7bfd188f567b0e18f43cdefb835c4d4f7c214a9
MK0mar0v <maksymkomarovofficial@gmail.com>
2026-02-19 15:45:37 -0800

    Merged all outputs into one sheet/excel file, added basic column sizing to avoid the worst of the cutoffs

--- utils/core/generate_excel.py
+++ utils/core/generate_excel.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import openpyxl
+from pathlib import Path
 from PyQt6.QtWidgets import QFileDialog
 import utils.logger.logger as log
 
@@ -18,14 +20,39 @@ def generate_excel(data):
     log.info(f"User selected path: {file_path[0]} -> Extension: {file_path[1]}")
 
     try:
-        #loop through list of dicts, treat every element as seperate table in excel sheet
+        #Create Empty List to store dataframes
+        dataframes = []
+        #df_titles = [] --Currently ununsed, might come back to add table titles 
+
+        #loop through list of dicts, treat every element as seperate table in excel sheet, collect in dataframe list
         for subsection, contents in data.items():
             #If nested list of elements get index number      
             table_index = range(len(contents)) if isinstance(contents,list) else [0]
+            dataframes.append(pd.DataFrame(contents, index=table_index))
+            #df_titles.append(f"{subsection}")
+        
+        sheet = Path(file_path[0]).name
+        vpadding = 2
 
-            dataframe = pd.DataFrame(contents, index=table_index)
-            dataframe.to_excel(f"{subsection}.xlsx")
+        with pd.ExcelWriter(file_path[0], "openpyxl") as excel_writer:
+            dataframes[0].to_excel(excel_writer, 
+                                   sheet_name = sheet, 
+                                   index_col = False)
+            for x in range(1, len(dataframes)):
+                dataframes[x].to_excel(excel_writer, 
+                                       sheet_name = sheet, 
+                                       startrow = excel_writer.sheets[sheet].max_row + vpadding, 
+                                       index_col = False)
+
+        #Resize columns to avoid cutoffs + potential additional styling
+        excel_file = openpyxl.load_workbook(file_path[0])
+        excel_sheet = excel_file[sheet]
+
+        for column in excel_sheet.columns:
+            excel_sheet.column_dimensions[column[0].column_letter].auto_size = True
+
+        excel_file.save(file_path[0])
     except Exception as e:
         log.error(f"Excel Creation failed: {e}")
-
+    
     return
