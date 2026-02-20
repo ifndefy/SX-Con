e819008e6ccae260232b08b9a7fa962743af6564
MK0mar0v <110884056+MK0mar0v@users.noreply.github.com>
2026-02-20 02:05:22 -0800

    SXC-15: Implement-Feature-Export-Button-action (#109)
    * First scaffolding added, updated pyproject with pandas dependency + new poetry.lock
    
    * fixed log path in config.ini, added comments
    
    * first implementation of excel conversion, new poetry lock (Importing openpyxl)
    
    * Added QfileDialog to generate_excel, changed excel.py structure
    
    * Merged all outputs into one sheet/excel file, added basic column sizing to avoid the worst of the cutoffs
    
    * linked export buttons in open_tickets.py
    
    * added export functionality to vendor_tickets.py

--- ui/tabs/create_new.py
+++ ui/tabs/create_new.py
@@ -17,8 +17,8 @@ from ui.tabs.base import BaseTab
 from ui.core.autogen_date import generate_host_datetime
 from ui.core.autogen_ticket_num import autogen_ticket_num
 from ui.core.revenue_generation import RevenueGeneration
-from ui.core import format_phone
-from ui.core import format_price
+from ui.core import format_phone, format_price, excel
+from utils.core import generate_excel as xls_gen
 import utils.logger.logger as log
 
 
@@ -287,11 +287,11 @@ class CreateNewTab(BaseTab):
         action_layout = QVBoxLayout()
 
         self.print_btn = QPushButton("Print")
-        self.export_btn = QPushButton("Export")
+        self.excel_btn = excel.ExcelButton(self.gather_record, xls_gen.generate_excel, "Excel")
         self.pdf_btn = QPushButton("PDF")
 
         action_layout.addWidget(self.print_btn)
-        action_layout.addWidget(self.export_btn)
+        action_layout.addWidget(self.excel_btn)
         action_layout.addWidget(self.pdf_btn)
 
         action_layout.addStretch()
@@ -479,6 +479,16 @@ class CreateNewTab(BaseTab):
 
         msg_box.exec()
         return msg_box.clickedButton() == confirm_btn
+
+    def gather_record(self):
+        vendor_info = self._gather_vendor_data()
+        product_info = self._gather_products_data()
+        revenue_data = self._gather_revenue_data()
+        return {
+            'vendor_info': vendor_info,
+            'prod_info': product_info,
+            'revenue_info': revenue_data
+        }
 
     def setup_button_connections(self):
         """
