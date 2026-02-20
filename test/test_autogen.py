d57435a9fdf1aefc6f2b96b987641a6d4cc341c9
Tyler-Slagboom <tylerslagboom@csus.edu>
2026-02-20 02:05:44 -0800

    SXC-186: create and fill test_autogen_date.py (#111)
    * create and fill test_autogen_date.py
    
    * SXC-186: merge resolution - combine both PR changes into a single file
    
    ---------
    
    Co-authored-by: Radioactivwaffl <tyler.slagboom@gmail.com>
    Co-authored-by: ifndefy <lee.sjoe.3@gmail.com>
    Co-authored-by: Joe <69750727+ifndefy@users.noreply.github.com>

--- test/test_autogen.py
+++ test/test_autogen.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 
 from services.connect_database import db_connection
 from ui.core.autogen_ticket_num import autogen_ticket_num
+from ui.core.autogen_date import generate_host_datetime
 
 def test_autogen_ticket_num():
-
     """
     purpose: test the accuracy of the autogen_ticket_num function
     return: 0 on success, else -1
@@ -34,3 +35,31 @@ def test_autogen_ticket_num():
 
     except Exception:
         return -1
+
+def test_autogen_date():
+
+    """
+    purpose: test the accuracy of the generate_host_datetime function
+    return: 0 on success, else -1
+    author: Tyler Slagboom
+    """
+
+    try:
+        now = datetime.now()
+        format = "%m/%d/%y -- %H:%M:%S"
+        now = now.strftime(format)
+        auto_time = generate_host_datetime(include_seconds=True)
+
+        print(now)
+        print(auto_time)
+
+        if now == auto_time:
+            return 0
+
+        return -1
+
+    except Exception:
+        return -1
+
+print(test_autogen_date())
+print(test_autogen_ticket_num())
