377fc029f93385b6f33e658cedfc338313ccfe64
ifndefy <lee.sjoe.3@gmail.com>
2026-02-20 02:02:48 -0800

    SXC-186: merge resolution - combine both PR changes into a single file

--- test/test_autogen.py
+++ test/test_autogen.py
@@ -1,6 +1,40 @@
-
 from datetime import datetime
+
+from services.connect_database import db_connection
+from ui.core.autogen_ticket_num import autogen_ticket_num
 from ui.core.autogen_date import generate_host_datetime
+
+def test_autogen_ticket_num():
+    """
+    purpose: test the accuracy of the autogen_ticket_num function
+    return: 0 on success, else -1
+    author: Tyler Slagboom
+    """
+
+    try:
+        container = db_connection.connect('Consignments')
+        property = 'ticket_number'
+
+        query = f"SELECT VALUE MAX(c.{property}) FROM c"
+
+        items = list(container.query_items(
+            query=query,
+            enable_cross_partition_query=True
+        ))
+
+        if items and items[0] is not None:
+            max_ticket = int(items[0])
+        else:
+            return -1
+
+        ticket_num = int(autogen_ticket_num())
+        if ticket_num - max_ticket == 1:
+            return 0
+
+        return -1
+
+    except Exception:
+        return -1
 
 def test_autogen_date():
 
@@ -26,3 +60,6 @@ def test_autogen_date():
 
     except Exception:
         return -1
+
+print(test_autogen_date())
+print(test_autogen_ticket_num())
