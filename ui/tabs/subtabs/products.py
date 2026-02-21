ccd691d2a9e2c2c0fbb184714b2f6bc854ddc21b
Tim Liu <158377504+Gentleman644@users.noreply.github.com>
2026-02-20 21:48:58 -0800

    SXC-244: implement create product window (#120)

--- ui/tabs/subtabs/products.py
+++ ui/tabs/subtabs/products.py
@@ -1,26 +1,29 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QVBoxLayout, QTabWidget
+from PyQt6.QtWidgets import QVBoxLayout
+from PyQt6.QtWidgets import QComboBox
 from PyQt6.QtWidgets import QHBoxLayout
 from PyQt6.QtWidgets import QLabel
 from PyQt6.QtWidgets import QPushButton
 from PyQt6.QtWidgets import QLineEdit
 from PyQt6.QtWidgets import QScrollArea
 from PyQt6.QtWidgets import QWidget
+from PyQt6.QtWidgets import QDialog
 
-from services.connect_database import db_connection
-from ui.tabs.base import BaseTab
-import utils.logger.logger as log
 from services.message_bus import status_bar_instance
+from ui.tabs.base import BaseTab
+
+from services.insert_item import insert_item
+import utils.logger.logger as log
 
 class ProductsTab(BaseTab):
     def __init__(self, api_handler, db_connection):
-        self.tickets_section = []
+        self.product_type = ['Hot_Food', 'General', 'Produce']
+        self.products_section = []
         self.db_connection = db_connection
         super().__init__(api_handler, "products")
 
     def setup_ui(self):
         """
-        :purpose: initializes the "Create New Record" tab
+        :purpose: initializes the "Create New Product" tab
         :return: None
         :author(s): Joe Lee
         """
@@ -33,7 +36,7 @@ class ProductsTab(BaseTab):
         # Line 0 Creation
         header_section = QHBoxLayout()
 
-        # Ticket Header
+        # Product Header
         title = QLabel("View Products")
         title.setObjectName("post_title")
         header_section.addWidget(title)
@@ -44,30 +47,30 @@ class ProductsTab(BaseTab):
         # Ends creation and adds header_section to window
         layout.addLayout(header_section)
 
-        # HR Line between Vendor and Tickets sections
+        # HR Line between Vendor and Products sections
         hr1 = QLabel()
         hr1.setObjectName("hr")
         layout.addWidget(hr1)
 
-        # Ticket Line 1: Tickets sections container
-        self.tickets_layout = QVBoxLayout()
+        # Product Line 1: Products sections container
+        self.products_layout = QVBoxLayout()
 
-        layout.addLayout(self.tickets_layout)
+        layout.addLayout(self.products_layout)
 
-        tickets_section = QHBoxLayout()
+        products_section = QHBoxLayout()
 
-        layout.addLayout(tickets_section)
+        layout.addLayout(products_section)
         layout.addStretch()
 
-        vendor_section_row_3 = QHBoxLayout()
+        product_section_row_3 = QHBoxLayout()
         self.create_btn = QPushButton("Create New Product")
-        vendor_section_row_3.addWidget(self.create_btn)
+        product_section_row_3.addWidget(self.create_btn)
 
-        vendor_section_row_3.addStretch()
+        product_section_row_3.addStretch()
 
         self.update_btn = QPushButton("Update")
-        vendor_section_row_3.addWidget(self.update_btn)
-        layout.addLayout(vendor_section_row_3)
+        product_section_row_3.addWidget(self.update_btn)
+        layout.addLayout(product_section_row_3)
 
         # HR Line to separate buttons at the bottom
         hr3 = QLabel()
@@ -81,20 +84,20 @@ class ProductsTab(BaseTab):
 
         self.setup_button_connections()
 
-    def add_user_section(self):
+    def add_product_section(self):
         """
-        :purpose: adds ticket lines
+        :purpose: adds product lines
         :return: None
         :author(s): Joe Lee
         """
-        tickets_section = {}
+        prods_section = {}
 
-        # Ticket section container
+        # product section container
         section_widget = QWidget()
         section_layout = QVBoxLayout(section_widget)
         section_layout.setContentsMargins(0, 0, 0, 0)
 
-        # Line 1: ticket_num + datetime + status + buttons
+        # Line 1: product_num + datetime + status + buttons
         line1_layout = QHBoxLayout()
 
         # user id
@@ -173,41 +176,41 @@ class ProductsTab(BaseTab):
         section_layout.addWidget(hr)
 
         # Add to container
-        self.tickets_layout.addWidget(section_widget)
-        self.tickets_section.append(tickets_section)
+        self.products_layout.addWidget(section_widget)
+        self.products_section.append(prods_section)
 
-    def remove_ticket_section(self):
+    def remove_product_section(self):
         """
-        :purpose: removes and clears all ticket sections
+        :purpose: removes and clears all product sections
         :return: None
         :author(s): Joe Lee
         """
         # Remove all widgets from the layout
-        for i in reversed(range(self.tickets_layout.count())):
-            widget = self.tickets_layout.itemAt(i).widget()
+        for i in reversed(range(self.products_layout.count())):
+            widget = self.products_layout.itemAt(i).widget()
             if widget:
-                self.tickets_layout.removeWidget(widget)
+                self.products_layout.removeWidget(widget)
                 widget.deleteLater()
 
-        # Clear the tickets_section list
-        self.tickets_section.clear()
+        # Clear the products_section list
+        self.products_section.clear()
 
         # Update status
-        status_bar_instance.send_message("All tickets cleared")
+        status_bar_instance.send_message("All products cleared")
 
     def fetch_on_clicked(self):
         """
-        :purpose: calls fetch method and adds ticket sections
+        :purpose: calls fetch method and adds product sections
         :return: None
         :author(s): Joe Lee
         """
-        self.remove_ticket_section()
+        self.remove_product_section()
         products = self.fetch()
         if not products:
             log.error("No products found")
         else:
             for product in products:
-                self.add_user_section()
+                self.add_product_section()
 
     def fetch(self):
         """
@@ -242,6 +245,95 @@ class ProductsTab(BaseTab):
             log.error(f"Error fetching products: {e}")
             return []
 
+    def create_new_product_prompt(self):
+        '''
+        :purpose: Sets up the UI and uses helper methods to create a product and insert it into the db
+        :author(s): Tim Liu
+        '''
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Create New Product")
+
+        layout = QVBoxLayout(dialog)
+
+        # Fields
+        layout.addWidget(QLabel("Product ID:"))
+        product_id_input = QLineEdit()
+        product_id_input.setObjectName("product_id_input")
+        layout.addWidget(product_id_input)
+
+        layout.addWidget(QLabel("Product Name:"))
+        product_name_input = QLineEdit()
+        product_name_input.setObjectName("product_name_input")
+        layout.addWidget(product_name_input)
+
+        prompt_label = QLabel("Product Type:")
+        layout.addWidget(prompt_label)
+        product_type_input= QComboBox()
+        product_type_input.addItems(self.product_type)
+        product_type_input.setObjectName("product_type_input")
+        product_type_input.setCurrentIndex(-1)
+        layout.addWidget(product_type_input)
+
+        layout.addWidget(QLabel("rate:"))
+        rate_input = QLineEdit()
+        rate_input.setObjectName("rate_input")
+        layout.addWidget(rate_input)
+
+        layout.addStretch()
+
+        # Buttons
+        btn_layout = QHBoxLayout()
+        create_btn = QPushButton("Create")
+        cancel_btn = QPushButton("Cancel")
+
+        btn_layout.addWidget(create_btn)
+        btn_layout.addWidget(cancel_btn)
+        layout.addLayout(btn_layout)
+
+        # Connects
+        create_btn.clicked.connect(self.on_create_clicked(dialog))
+        cancel_btn.clicked.connect(dialog.reject)
+
+        # Execute dialog
+        result = dialog.exec()
+        if result == QDialog.DialogCode.Accepted:
+            self.handle_dialog_accepted(dialog)
+
+    def on_create_clicked(self, dialog):
+        """
+        :Purpose: handles button initialization
+        :Author(s): Tim Liu
+        """
+        def handler():
+            if self.validate_product_data(dialog):
+                dialog.accept()
+        return handler
+
+    def handle_dialog_accepted(self, dialog):
+        """
+        :Purpose: executes a sequence of events
+        :Author(s): Joe Lee
+        """
+        self.new_product_data = self.gather_new_product_data(dialog)
+        insert_item("Entities", "product", self.new_product_data)
+
+    def gather_new_product_data(self, dialog):
+        """
+        :Purpose: gathers user data from dialog's input fields
+        :Method: passes in dialog then parses dialog for data
+        :Author(s): Colin Heinselman, Joe Lee
+        """
+        raw_product_data = {
+            "product_id": dialog.findChild(QLineEdit, "product_id_input").text(),
+            "product_name": dialog.findChild(QLineEdit, "product_name_input").text(),
+            "product_type": dialog.findChild(QComboBox, "product_type_input").currentText(),
+            "rate": dialog.findChild(QLineEdit, "rate_input").text()
+        }
+        return raw_product_data
+
+    def validate_product_data(self, dialog):
+        return True
+
     def setup_button_connections(self):
         """
         :purpose: links buttons with methods
@@ -249,3 +341,4 @@ class ProductsTab(BaseTab):
         :author(s): Joe Lee
         """
         self.update_btn.clicked.connect(self.fetch_on_clicked)
+        self.create_btn.clicked.connect(self.create_new_product_prompt)
