3f48749504885d1ca52df25ec61b87a7ea963b19
Joe <69750727+ifndefy@users.noreply.github.com>
2026-02-20 17:32:48 -0800

    SXC-162: ports user creation from uesr.py to vendor creation in vendors.py (Admin Settings subtabs) (#119)

--- ui/tabs/subtabs/vendors.py
+++ ui/tabs/subtabs/vendors.py
@@ -1,5 +1,5 @@
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QVBoxLayout, QTabWidget
+
+from PyQt6.QtWidgets import QVBoxLayout, QDialog
 from PyQt6.QtWidgets import QHBoxLayout
 from PyQt6.QtWidgets import QLabel
 from PyQt6.QtWidgets import QPushButton
@@ -7,21 +7,22 @@ from PyQt6.QtWidgets import QLineEdit
 from PyQt6.QtWidgets import QScrollArea
 from PyQt6.QtWidgets import QWidget
 
-from services.connect_database import db_connection
 from ui.tabs.base import BaseTab
+
+from services.insert_item import insert_item
 import utils.logger.logger as log
 from services.message_bus import status_bar_instance
 
 class VendorsTab(BaseTab):
     def __init__(self, api_handler, db_connection):
-        self.tickets_section = []
+        self.new_vendor_data = None
+        self.vendors_section = []
         self.db_connection = db_connection
         super().__init__(api_handler, "vendors")
 
     def setup_ui(self):
         """
-        :purpose: initializes the "Create New Record" tab
-        :return: None
+        :purpose: initializes the "vendors subtab" tab
         :author(s): Joe Lee
         """
         # Enable scrolling for when the content exceeds the height of the window
@@ -30,46 +31,35 @@ class VendorsTab(BaseTab):
         scroll_content = QWidget()
         layout = QVBoxLayout(scroll_content)
 
-        # Line 0 Creation
         header_section = QHBoxLayout()
-
-        # Ticket Header
-        title = QLabel("View Vendors")
+        title = QLabel("View Vendors") # Subtab header
         title.setObjectName("post_title")
         header_section.addWidget(title)
+        header_section.addStretch() # Push to the left
+        layout.addLayout(header_section) # Ends creation and adds header_section to window
 
-        # Push to the left
-        header_section.addStretch()
-
-        # Ends creation and adds header_section to window
-        layout.addLayout(header_section)
-
-        # HR Line between Vendor and Tickets sections
-        hr1 = QLabel()
+        hr1 = QLabel()# HR Line
         hr1.setObjectName("hr")
         layout.addWidget(hr1)
 
-        # Ticket Line 1: Tickets sections container
-        self.tickets_layout = QVBoxLayout()
-
-        layout.addLayout(self.tickets_layout)
+        self.vendors_layout = QVBoxLayout() # Vendors container
+        layout.addLayout(self.vendors_layout)
 
         tickets_section = QHBoxLayout()
-
         layout.addLayout(tickets_section)
         layout.addStretch()
 
-        vendor_section_row_3 = QHBoxLayout()
+        btn_section = QHBoxLayout()
         self.create_btn = QPushButton("Create New Vendor")
-        vendor_section_row_3.addWidget(self.create_btn)
+        btn_section.addWidget(self.create_btn)
+        self.create_btn.clicked.connect(self.create_new_vendor_prompt)
 
-        vendor_section_row_3.addStretch()
+        btn_section.addStretch()
 
         self.update_btn = QPushButton("Update")
-        vendor_section_row_3.addWidget(self.update_btn)
-        layout.addLayout(vendor_section_row_3)
+        btn_section.addWidget(self.update_btn)
+        layout.addLayout(btn_section)
 
-        # HR Line to separate buttons at the bottom
         hr3 = QLabel()
         hr3.setObjectName("hr")
         layout.addWidget(hr3)
@@ -81,23 +71,19 @@ class VendorsTab(BaseTab):
 
         self.setup_button_connections()
 
-    def add_user_section(self):
+    def add_vendor_section(self):
         """
-        :purpose: adds ticket lines
-        :return: None
+        :purpose: adds vendor items
         :author(s): Joe Lee
         """
-        tickets_section = {}
+        vendors_section = {}
 
-        # Ticket section container
+        # Vendors section container
         section_widget = QWidget()
         section_layout = QVBoxLayout(section_widget)
         section_layout.setContentsMargins(0, 0, 0, 0)
 
-        # Vendor Line 1 Creation: Vendor ID + Phone Number + Date and Time
         vendor_section_row_1 = QHBoxLayout()
-
-        # Vendor ID
         vendor_section_row_1.addWidget(QLabel("Vendor ID:"))
         vendor_id_input = QLineEdit()
         vendor_id_input.setPlaceholderText("4 INTS")
@@ -226,27 +212,8 @@ class VendorsTab(BaseTab):
         section_layout.addWidget(hr)
 
         # Add to container
-        self.tickets_layout.addWidget(section_widget)
-        self.tickets_section.append(tickets_section)
-
-    def remove_ticket_section(self):
-        """
-        :purpose: removes and clears all ticket sections
-        :return: None
-        :author(s): Joe Lee
-        """
-        # Remove all widgets from the layout
-        for i in reversed(range(self.tickets_layout.count())):
-            widget = self.tickets_layout.itemAt(i).widget()
-            if widget:
-                self.tickets_layout.removeWidget(widget)
-                widget.deleteLater()
-
-        # Clear the tickets_section list
-        self.tickets_section.clear()
-
-        # Update status
-        status_bar_instance.send_message("All tickets cleared")
+        self.vendors_layout.addWidget(section_widget)
+        self.vendors_section.append(vendors_section)
 
     def fetch_on_clicked(self):
         """
@@ -260,7 +227,7 @@ class VendorsTab(BaseTab):
             log.error("No vendors found")
         else:
             for vendor in vendors:
-                self.add_user_section()
+                self.add_vendor_section()
 
     def fetch(self):
         """
@@ -272,8 +239,7 @@ class VendorsTab(BaseTab):
             container = self.db_connection.connect("Entities")
 
             query = """
-            SELECT c.id, c.vendor_id, c.phone, c.first_name, c.middle_name, 
-                   c.last_name, c.address, c.city, c.state, c.zip_code
+            SELECT *
             FROM c
             WHERE c.type = 'vendor'
             ORDER BY c.vendor_id ASC
@@ -310,3 +276,121 @@ class VendorsTab(BaseTab):
         :author(s): Joe Lee
         """
         self.update_btn.clicked.connect(self.fetch_on_clicked)
+
+    def create_new_vendor_prompt(self):
+        '''
+        :purpose: Sets up the UI and uses helper methods to create a user and insert it into the db
+        :author(s): Colin Heinselman
+        '''
+        dialog = QDialog(self)
+        dialog.setWindowTitle("Create New Vendor")
+
+        layout = QVBoxLayout(dialog)
+
+        # Fields
+        layout.addWidget(QLabel("VendorID:"))
+        vendor_id_input = QLineEdit()
+        vendor_id_input.setObjectName("vendor_id_input")
+        layout.addWidget(vendor_id_input)
+
+        layout.addWidget(QLabel("Phone Number:"))
+        phone_number_input = QLineEdit()
+        phone_number_input.setObjectName("phone_number_input")
+        layout.addWidget(phone_number_input)
+
+        layout.addWidget(QLabel("First Name:"))
+        first_name_input = QLineEdit()
+        first_name_input.setObjectName("first_name_input")
+        layout.addWidget(first_name_input)
+
+        layout.addWidget(QLabel("Middle Name:"))
+        middle_name_input = QLineEdit()
+        middle_name_input.setObjectName("middle_name_input")
+        layout.addWidget(middle_name_input)
+
+        layout.addWidget(QLabel("Last Name:"))
+        last_name_input = QLineEdit()
+        last_name_input.setObjectName("last_name_input")
+        layout.addWidget(last_name_input)
+
+        layout.addWidget(QLabel("Address:"))
+        address_input = QLineEdit()
+        address_input.setObjectName("address_input")
+        layout.addWidget(address_input)
+
+        layout.addWidget(QLabel("City:"))
+        city_input = QLineEdit()
+        city_input.setObjectName("city_input")
+        layout.addWidget(city_input)
+
+        layout.addWidget(QLabel("State:"))
+        state_input = QLineEdit()
+        state_input.setObjectName("state_input")
+        layout.addWidget(state_input)
+
+        layout.addWidget(QLabel("Zip Code:"))
+        zip_code_input = QLineEdit()
+        zip_code_input.setObjectName("zip_code_input")
+        layout.addWidget(zip_code_input)
+
+        layout.addStretch()
+
+        # Buttons
+        btn_layout = QHBoxLayout()
+        create_btn = QPushButton("Create")
+        cancel_btn = QPushButton("Cancel")
+
+        btn_layout.addWidget(create_btn)
+        btn_layout.addWidget(cancel_btn)
+        layout.addLayout(btn_layout)
+
+        # Connects
+        create_btn.clicked.connect(self.on_create_clicked(dialog))
+        cancel_btn.clicked.connect(dialog.reject)
+
+        # Execute dialog
+        result = dialog.exec()
+        if result == QDialog.DialogCode.Accepted:
+            self.handle_dialog_accepted(dialog)
+
+    def on_create_clicked(self, dialog):
+        """
+        :Purpose: handles button initialization
+        :Author(s): Joe Lee
+        """
+        def handler():
+            if self.validate_vendor_data(dialog):
+                dialog.accept()
+        return handler
+
+    def handle_dialog_accepted(self, dialog):
+        """
+        :Purpose: executes a sequence of events
+        :Author(s): Joe Lee
+        """
+        self.new_vendor_data = self.gather_vendor_data(dialog)
+        print(self.new_vendor_data)
+        insert_item("Entities", "vendor", self.new_vendor_data)
+
+    def gather_vendor_data(self, dialog):
+        """
+        :Purpose: gathers user data from dialog's input fields
+        :Method: passes in dialog then parses dialog for data
+        :Author(s): Colin Heinselman, Joe Lee
+        """
+        vendor_doc = {
+            "vendor_id": dialog.findChild(QLineEdit, "vendor_id_input").text(),
+            "phone": dialog.findChild(QLineEdit, "phone_number_input").text(),
+            "first_name": dialog.findChild(QLineEdit, "first_name_input").text(),
+            "middle_name": dialog.findChild(QLineEdit, "middle_name_input").text(),
+            "last_name": dialog.findChild(QLineEdit, "last_name_input").text(),
+            "address": dialog.findChild(QLineEdit, "address_input").text(),
+            "city": dialog.findChild(QLineEdit, "city_input").text(),
+            "state": dialog.findChild(QLineEdit, "state_input").text(),
+            "zip_code": dialog.findChild(QLineEdit, "zip_code_input").text(),
+        }
+        return vendor_doc
+
+    def validate_vendor_data(self, dialog):
+        # todo:
+        return True
