1040ad6f3f2eec43e606f2edd6971d53f1fec104
ifndefy <lee.sjoe.3@gmail.com>
2026-02-27 00:04:46 -0800

    SXC-224: remove debugging code from SXC

--- SXC.py
+++ SXC.py
@@ -1,31 +1,4 @@
 import sys
-import traceback
-import builtins  # also hook original print if needed
-
-original_write = sys.stdout.write
-
-def trace_write(text):
-    if '8' in text:
-        # Write to stderr to avoid recursion
-        sys.stderr.write("=== 8 printed from ===\n")
-        traceback.print_stack(file=sys.stderr)
-    return original_write(text)
-
-sys.stdout.write = trace_write
-
-# Also hook the built-in print (calls sys.stdout.write, but just in case)
-original_print = builtins.print
-def trace_print(*args, **kwargs):
-    if any('8' in str(arg) for arg in args):
-        sys.stderr.write("=== print(8) called from ===\n")
-        traceback.print_stack(file=sys.stderr)
-    return original_print(*args, **kwargs)
-
-builtins.print = trace_print
-
-# Rest of your imports and code below...
-
-import sys
 import utils.logger as log
 
 from PyQt6.QtWidgets import QApplication, QDialog
