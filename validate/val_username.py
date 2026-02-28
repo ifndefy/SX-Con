fffec224e55e8e9f0bc1088f9a23351855e6b4b3
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2026-02-28 04:00:16 -0800

    Update val_username.py
    Clean up comments and remove testing code

--- validate/val_username.py
+++ validate/val_username.py
@@ -3,7 +3,7 @@ def val_username(username: str) -> int:
     max_length = 18
     min_length = 4
 
-    # Allows username to include upper and lowercase letters, numbers, underscore (_), and period (.)
+    # Allows username to include only upper and lowercase letters, as well as numbers.
     allowed_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
     if len(username) > max_length or len(username) < min_length:
@@ -13,10 +13,4 @@ def val_username(username: str) -> int:
         return -1
 
     return 0
-
-
-print(val_username("123"))
-print(val_username("1234567890123456789"))
-print(val_username("*username"))
-print(val_username("username"))
 
