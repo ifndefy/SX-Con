15420d6175fa981d4901d52294bc7231b5851e88
ColinH13 <117141355+ColinH13@users.noreply.github.com>
2026-02-28 16:03:37 -0800

    Update val_password.py
    Added docstring

--- validate/val_password.py
+++ validate/val_password.py
@@ -1,5 +1,12 @@
 
 def val_password(password: str) -> int:
+    """
+    :purpose:Validates password against max_length and min_length requirements.
+    :param password: The String password to validate.
+    :return: 0 if valid, -1 if invalid.
+    :author(s): Colin Heinselman
+    """
+
     # Set Conditions
     max_length = 25
     min_length = 4
