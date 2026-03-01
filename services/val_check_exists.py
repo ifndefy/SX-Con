2f10661a216db6719f2dc988a77b0101437762ec
crhen <crhender8@gmail.com>
2026-03-01 13:32:51 -0800

    added comments, changed file name, tested working

--- services/val_check_exists.py
+++ services/val_check_exists.py
@@ -3,6 +3,10 @@
 from services.get_item_by_property import get_item_by_property
 
 def val_check_exists(value: str):
+    """
+    accepts a single string value for product name and checks if the product exists
+    Author(s): Colin Henderson
+    """
     result = get_item_by_property(
         "Entities",
         "product",
