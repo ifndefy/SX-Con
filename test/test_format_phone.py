97a1e560d3fb5751cc91d77b8dc7869c3f8d9296
Joe <69750727+ifndefy@users.noreply.github.com>
2026-03-03 23:39:29 -0800

    SXC-188: implement test for format_phone (#156)
    * SXC-188: implement test for format_phone
    
    * SXC-188: add AssertError

---
+++ test/test_format_phone.py
@@ -0,0 +1,51 @@
+"""
+tests if instances of format_phone are correctly masking inputs
+"""
+import sys
+from PyQt6.QtWidgets import QApplication
+
+from ui.core.format_phone import PhoneNumField
+
+def test_format_phone():
+    app = QApplication(sys.argv)
+    test_phone_field = PhoneNumField()
+
+    test_phone_field.setText("1")
+    test_phone_field.textEdited.emit("1")
+    assert test_phone_field.text() == "1--", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("12")
+    test_phone_field.textEdited.emit("12")
+    assert test_phone_field.text() == "12--", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("123")
+    test_phone_field.textEdited.emit("123")
+    assert test_phone_field.text() == "123--", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("1234")
+    test_phone_field.textEdited.emit("1234")
+    assert test_phone_field.text() == "123-4-", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("12345")
+    test_phone_field.textEdited.emit("12345")
+    assert test_phone_field.text() == "123-45-", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("123456")
+    test_phone_field.textEdited.emit("123456")
+    assert test_phone_field.text() == "123-456-", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("1234567")
+    test_phone_field.textEdited.emit("1234567")
+    assert test_phone_field.text() == "123-456-7", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("12345678")
+    test_phone_field.textEdited.emit("12345678")
+    assert test_phone_field.text() == "123-456-78", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("123456789")
+    test_phone_field.textEdited.emit("123456789")
+    assert test_phone_field.text() == "123-456-789", "Assert: phone number failed to be masked"
+
+    test_phone_field.setText("1234567890")
+    test_phone_field.textEdited.emit("1234567890")
+    assert test_phone_field.text() == "123-456-7890", "Assert: phone number failed to be masked"
