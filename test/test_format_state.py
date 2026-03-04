074355ea495a3494a5cd32a7492a4e2099983024
ifndefy <lee.sjoe.3@gmail.com>
2026-03-03 23:38:29 -0800

    SXC-311: add AssertError

--- test/test_format_state.py
+++ test/test_format_state.py
@@ -12,28 +12,28 @@ def test_format_state():
 
     test_state_field.setText("a")
     test_state_field.textEdited.emit("a")
-    assert test_state_field.text() == "A"
+    assert test_state_field.text() == "A", "Assert: Failed to capitalize"
 
     test_state_field.setText("")
     test_state_field.textEdited.emit("")
-    assert test_state_field.text() == ""
+    assert test_state_field.text() == "", "Assert: Failed to clear field"
 
     test_state_field.setText("ab")
     test_state_field.textEdited.emit("ab")
-    assert test_state_field.text() == "AB"
+    assert test_state_field.text() == "AB", "Assert: Failed to capitalize"
 
     test_state_field.setText("a")
     test_state_field.textEdited.emit("a")
-    assert test_state_field.text() == "A"
+    assert test_state_field.text() == "A", "Assert: Failed to capitalize"
 
     test_state_field.setText("abc")
     test_state_field.textEdited.emit("abc")
-    assert test_state_field.text() == "AB"
+    assert test_state_field.text() == "AB", "Assert: Failed to capitalize"
 
     test_state_field.setText("1")
     test_state_field.textEdited.emit("1")
-    assert test_state_field.text() == ""
+    assert test_state_field.text() == "", "Assert: Failed to prevent integer input"
 
     test_state_field.setText("*")
     test_state_field.textEdited.emit("*")
-    assert test_state_field.text() == ""
+    assert test_state_field.text() == "", "Assert: Failed to prevent special character input"
