5969e12d9e2877132e06cb3fc35465e66e7813b3
crhen <crhender8@gmail.com>
2026-03-11 20:35:00 -0700

    SXC-356-implement-tests

---
+++ test/ult/test_status_bar.py
@@ -0,0 +1,7 @@
+import pytest
+from unittest.mock import MagicMock
+from ui.core.status_bar import StatusBar
+
+def test_status_bar_exists(app):
+    statbar = StatusBar("test")
+    assert statbar is not None
