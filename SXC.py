cbe0d02bdccb664cef532711d63953e3b32c0be7
Joe <69750727+ifndefy@users.noreply.github.com>
2026-03-12 02:03:18 -0700

    SXC-372: update login loop to enable offline mode (#207)
    * SXC-372: enable only createnewtab when in offline mode
    
    * SXC-372: move looping around to enable offline mode

--- SXC.py
+++ SXC.py
@@ -1,21 +1,33 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtWidgets import QDialog
+
 from ui.main_window import MainWindow
 from ui.prompts.login import LoginScreen
 from ui.core.theme_manager import ThemeManager
+
+from services.connect_database import db_connection
+from src.user import current_user
 
 def main():
     app = QApplication(sys.argv)
     theme_manager = ThemeManager()
 
     while True:
-        login_screen = LoginScreen(theme_manager)
-        if login_screen.exec() != QDialog.DialogCode.Accepted:
-            break
+        db_connection.__init__()
+        if db_connection.connect("Consignments"):
+            login_screen = LoginScreen(theme_manager)
+            if login_screen.exec() != QDialog.DialogCode.Accepted:
+                break
+            window = MainWindow()
+        else:
+            QMessageBox.information(None, "Error", "Failed to connect to database\n"
+                                                   "Initializing in OFFLINE Mode")
+            current_user.set_user("offline", False)
+            window = MainWindow(offline_mode=True)
 
-        window = MainWindow()
         window.show()
         app.exec()
         if hasattr(window, 'logout_requested') and window.logout_requested:
