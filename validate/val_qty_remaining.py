0f6c9a4814dd80ec81de093fd09ce375bbc6fa11
Tim Liu <timliu@csus.edu>
2026-03-17 15:06:01 -0700

    SXC-300 implemented a validation method for remaining quantity

---
+++ validate/val_qty_remaining.py
@@ -0,0 +1,18 @@
+def val_qty_sold(rem: int, qty: int) -> bool:
+    """
+        purpose: validate that the remaining is appropriate to quantity
+        author(s): Tim Liu
+        parm rem: the remaining amount
+        param qty: the quantity in stock
+        return: True if valid, False otherwise
+    """
+    if rem is not int:
+        return False
+
+    if rem > qty:
+        return False
+
+    if rem < 0:
+        return False
+
+    return True
