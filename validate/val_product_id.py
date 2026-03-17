e375a05770d86acf8d1a731c2483091667e51e87
crhen <crhender8@gmail.com>
2026-03-16 19:02:02 -0700

    SXC-290-VAL---val_product_id

---
+++ validate/val_product_id.py
@@ -0,0 +1,39 @@
+from validate.val_check_does_not_exist import val_check_does_not_exists
+
+
+def val_product_id(prod_id: int) -> bool:
+    """
+    validates product ID before insertion into database
+
+    returns False if:
+        - value is None
+        - contains alphabet
+        - contains special characters
+        - already exists in DB
+
+    else returns True
+    Author(s):
+    Colin Henderson
+    """
+
+    if prod_id is None:
+        return False
+
+    prod_id_str = str(prod_id)
+
+    # Must contain only digits
+    if not prod_id_str.isdigit():
+        return False
+
+    # Must not already exist in DB
+    exists_check = val_check_does_not_exists(
+        "Entities",
+        "product",
+        "product_id",
+        prod_id
+    )
+
+    if exists_check == -1:
+        return False
+
+    return True
