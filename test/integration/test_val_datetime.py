deadcceb9fb252773e5a73145c48b8d542f04ce5
Tim Liu <timliu@csus.edu>
2026-03-18 00:15:57 -0700

    added assert strings for what went wrong

--- test/integration/test_val_datetime.py
+++ test/integration/test_val_datetime.py
@@ -6,19 +6,19 @@ def test_val_datetime():
         author(s): Tim Liu
     """
     if val_datetime(''):
-        assert False
+        assert False, 'Unexpected empty string passed in method'
 
     if val_datetime(' -- '):
-        assert False
+        assert False, 'Unexpected empty 2 string passed in method'
 
     if val_datetime('wrong -- wrong'):
-        assert False
+        assert False, 'Unexpected 2 wrong string passed in method'
 
     if val_datetime('12/12/12 -- wrong'):
-        assert False
+        assert False, 'Unexpected time wrong string passed in method'
 
     if val_datetime('wrong -- 12:12'):
-        assert False
+        assert False, 'Unexpected date wrong string passed in method'
 
     if not val_datetime('12/12/12 -- 12:12'):
-        assert False
+        assert False, 'Unexpected correct date and time failed in method'
