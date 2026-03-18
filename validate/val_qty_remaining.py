bddc69a13e3d3cc104687d9a12994a587d9dec27
Tim Liu <timliu@csus.edu>
2026-03-18 00:49:45 -0700

    fix the check for int

--- validate/val_qty_remaining.py
+++ validate/val_qty_remaining.py
@@ -6,7 +6,7 @@ def val_qty_sold(rem: int, qty: int) -> bool:
         param qty: the quantity in stock
         return: True if valid, False otherwise
     """
-    if rem is not int:
+    if not isinstance(rem, int):
         return False
 
     if rem > qty:
