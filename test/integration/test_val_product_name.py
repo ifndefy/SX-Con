773784ff14a6f2af7417146385d77ce1edf70028
ifndefy <lee.sjoe.3@gmail.com>
2026-03-19 02:09:59 -0700

    SXC-332: fix tests, remove duplicate tests

--- test/integration/test_val_product_name.py
+++ test/integration/test_val_product_name.py
@@ -3,9 +3,7 @@ from services.get_item_by_property import get_item_by_property
 from validate.val_product_name import val_product_name
 def test_val_product_name():
 
-    assert val_product_name("uniquename") is True
-    assert val_product_name(None) is False
-    assert val_product_name("Product123") is False
-    assert val_product_name("Product!") is False
-    assert val_product_name("Product_Name") is False
-    assert val_product_name("Bananas") is False
+    assert val_product_name("uniquename") is True, "Expected 'uniquename' to be valid"
+    assert val_product_name(None) is False, "Expected None input to be invalid"
+    assert val_product_name("Product123") is False, "Expected 'Product123' to be invalid for integers"
+    assert val_product_name("Product!") is False, "Expected 'Product!' to be invalid for special character"
