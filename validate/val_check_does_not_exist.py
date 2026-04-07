5e8b8a49eaca5e492720941a01d0116c5d0be191
crhen <crhender8@gmail.com>
2026-04-06 17:34:37 -0700

    SXC-126-Documentation-of-code-(4) think I got them all, I'll check through again later

--- validate/val_check_does_not_exist.py
+++ validate/val_check_does_not_exist.py
@@ -11,7 +11,7 @@ def val_check_does_not_exists(
     value: Union[str, int]
 ) -> int:
     """
-    Validates that a property value doesn't already exist
+    Purpose: Validates that a property value doesn't already exist
 
     Returns:
         True -> value does not exist
